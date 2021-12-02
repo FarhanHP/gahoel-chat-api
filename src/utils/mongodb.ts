87d11c9bb92d2a8452b64b38b56f571d7e696f7e
farhanhp <farhan.herdia123@gmail.com>
2021-12-02 20:20:22 +0700

    chore: add credentials

--- src/utils/mongodb.ts
+++ src/utils/mongodb.ts
@@ -1,8 +1,8 @@
 import { MongoClient } from 'mongodb';
 
-const PASSWORD = ;
-const USERNAME = ;
-const DATABASE_NAME = ;
+const PASSWORD = 'c1FdLMKU98dcz0IT';
+const USERNAME = 'farhanhp';
+const DATABASE_NAME = 'gahoel-chat';
 const URI = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.y35vz.mongodb.net/${DATABASE_NAME}?retryWrites=true&w=majority`
 
 export const connectClient = async () => {
