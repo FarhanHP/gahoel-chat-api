bb5d266464b77c265300561c817b589e1e0f97cc
farhanhp <farhan.herdia123@gmail.com>
2021-12-02 20:08:47 +0700

    chore: remove mongodb credentials

--- src/utils/mongodb.ts
+++ src/utils/mongodb.ts
@@ -1,8 +1,8 @@
 import { MongoClient } from 'mongodb';
 
-const PASSWORD = 'Nanas3111()';
-const USERNAME = 'farhanhp';
-const DATABASE_NAME = 'gahoel-chat';
+const PASSWORD = ;
+const USERNAME = ;
+const DATABASE_NAME = ;
 const URI = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.y35vz.mongodb.net/${DATABASE_NAME}?retryWrites=true&w=majority`
 
 export const connectClient = async () => {
